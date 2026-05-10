c3ba2513504ff155b6082f37bc6881f5633412eb
jaeyeopme <jaeyeopme@gmail.com>
2026-05-10 14:41:00 +0900

    chore(commit): add commitlint tooling

---
+++ commitlint.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'test', 'refactor', 'build', 'chore', 'ci', 'perf'],
+    ],
+    'scope-case': [2, 'always', 'kebab-case'],
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [0],
+    'footer-max-line-length': [0],
+  },
+};
